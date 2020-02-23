394d32da07ff7cc5789b3704fd3a910f76e72bb3
unknown <985968134@qq.com>
2020-02-23 13:08:35 +0800

    wf

---
+++ precache-manifest.3766a03226b2f39d1267ca303991721d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ea5ab6599b91183db9b6bf19f869e1",
+    "url": "/wf/index.html"
+  },
+  {
+    "revision": "3b38fc2c913b71e7ae5a",
+    "url": "/wf/static/css/main.6719804c.chunk.css"
+  },
+  {
+    "revision": "0cdc99a33b761cf32c2e",
+    "url": "/wf/static/js/2.76ffc8a9.chunk.js"
+  },
+  {
+    "revision": "ae6c8816a8e2d524a6389c1cc708e651",
+    "url": "/wf/static/js/2.76ffc8a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b38fc2c913b71e7ae5a",
+    "url": "/wf/static/js/main.063b536e.chunk.js"
+  },
+  {
+    "revision": "9c066f5d8e3e3b0231d4",
+    "url": "/wf/static/js/runtime-main.6526eb7b.js"
+  }
+]);
